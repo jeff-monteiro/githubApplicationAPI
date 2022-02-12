00a65e6a8508b14d0f0233f2657de3ef19e72698
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-02-12 19:02:54 -0300

    listOfRepos

--- src/components/reposContainer/styles.js
+++ src/components/reposContainer/styles.js
@@ -1,23 +1,57 @@
 import styled from 'styled-components'
 
-Section, Title, ListReposContainer, Repo
 
-export const Section = styled.div`
+export const Section = styled.section`
+  width: 400px;
+  height: 400px;
+  background: rgba(255,255,255,.95);
+  border-radius: 8px;
+  border-bottom: 8px dotted #3492eb;
+  margin-top: 15px;
 
-
+  @media (max-width: 430px) {
+    width: 300px
+  }
 `
 
-export const Title = styled.div`
-
-
+export const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 10px;
+  color: #444;
+  width: 100%;
+  text-align: center;
+  height: 30px;
 `
 
-export const ListReposContainer = styled.div`
-
-
+export const ListReposContainer = styled.h1`
+  height: 350px;
+  width: 100%;
+  padding: 10px;
+  overflow-y: scroll;
 `
 
-export const Repo = styled.div`
-
-
+export const Repo = styled.h2`
+  width: 100%;
+  height: 80px;
+  background: rgba(0,0,0,.05);
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+  h2 {
+    font-size: 15px;
+    width: 100%;
+    overflow: hidden;
+    color: #444;
+  }
+  p {
+    font-size: 12px;
+    font-weight: 400;
+    color: #777;
+    width: 100%;
+    height: 30px;
+    overflow: hidden;
+    line-height: 1.5;
+    margin-top: 10px;
+  }
 `
